302bf1f821a271b918d5f608d8688462e24b9ea2
Yoran Brondsema <yoran.brondsema@gmail.com>
2023-11-20 14:48:24 -0500

    chore: update treesitter

--- nvim/modules/treesitter.lua
+++ nvim/modules/treesitter.lua
@@ -20,11 +20,11 @@ require'nvim-treesitter.configs'.setup {
     "vim",
   },
   highlight = {
-    enable = true,              -- false will disable the whole extension
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
+    enable = true,
+    disable = {
+      -- we use vim-terraform for syntax coloring
+      "terraform"
+    },
     additional_vim_regex_highlighting = false
   },
   indent = {
