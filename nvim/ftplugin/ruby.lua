1fb12eccfa3e63a798e05872fbe8caa67aad390f
Yoran Brondsema <yoran.brondsema@gmail.com>
2024-08-05 10:39:40 +0200

    feat(neovim): add task in Overseer to execute rspec

---
+++ nvim/ftplugin/ruby.lua
@@ -0,0 +1,8 @@
+local rspec = require("custom.rspec")
+
+vim.api.nvim_set_keymap(
+  "n",
+  "<leader>tc",
+  ':lua require("custom.rspec").run_rspec_current_file()<CR>',
+  { noremap = true, silent = true }
+)
