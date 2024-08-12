895cde20788dca92eaec9e2741ef4fc029e7a851
Yoran Brondsema <yoran.brondsema@gmail.com>
2024-08-12 16:55:51 +0200

    feat(neovim): more customisations

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,3 +4,13 @@ vim.opt.wrap = true
 vim.g.lazyvim_prettier_needs_config = true
 
 vim.opt.relativenumber = false
+
+-- Ruby
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "Thorfile",
+  command = "setlocal ft=ruby",
+})
+vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+  pattern = "*.thor",
+  command = "setlocal ft=ruby",
+})
