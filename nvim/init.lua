2a539bc50354ee04038f15ba59780babb6924c53
Yoran Brondsema <yoran.brondsema@gmail.com>
2024-08-25 10:07:21 +0200

    feat(neovim): replace Copilot with Supermaven

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,10 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-vim.opt.colorcolumn = "100"
-vim.cmd([[
-  highlight ColorColumn ctermbg=red guibg=#ffcccc
-]])
-
 vim.opt.relativenumber = false
 vim.opt.wrap = true
+
+vim.opt.colorcolumn = "100"
+vim.cmd([[
+  highlight ColorColumn ctermbg=0 guibg=#555555
+]])
