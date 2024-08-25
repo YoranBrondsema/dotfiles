ecd082526fc2e4bba5b742b8ae0fb1319f20ac2f
Yoran Brondsema <yoran.brondsema@gmail.com>
2024-08-25 10:19:54 +0200

    feat(neovim): remove some unused plugins

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,6 +1,10 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
   { "rcarriga/nvim-notify", enabled = false },
-  { "hrsh7th/nvim-cmp", enabled = false },
   { "lewis6991/gitsigns.nvim", enabled = false },
+  -- https://www.lazyvim.org/plugins/coding
+  { "hrsh7th/nvim-cmp", enabled = false },
+  { "hrsh7th/cmp-buffer", enabled = false },
+  { "hrsh7th/cmp-nvim-lsp", enabled = false },
+  { "hrsh7th/cmp-path", enabled = false },
 }
