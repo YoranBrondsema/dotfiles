c7324fd687f6be95d3cba173ecc96af6f0a588c9
Yoran Brondsema <yoran.brondsema@gmail.com>
2024-11-19 15:39:49 +0100

    perf(neovim): turn off LSP logging

--- nvim/init.lua
+++ nvim/init.lua
@@ -8,3 +8,6 @@ vim.opt.colorcolumn = "100"
 vim.cmd([[
   highlight ColorColumn ctermbg=0 guibg=#555555
 ]])
+
+-- turn off LSP logging cause it brings the system to a halt
+vim.lsp.set_log_level("off")
