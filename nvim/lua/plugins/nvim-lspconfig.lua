ba65efa5d7bb1d2d6301e915e2ccb54bcb6ade97
Yoran Brondsema <yoran.brondsema@gmail.com>
2025-01-22 10:39:11 +0100

    feat(neovim): disable sorbet and ruby_lsp for Sutori

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,5 +1,9 @@
 local lspconfig = require("lspconfig")
 
+local function is_sutori_backend(root_dir)
+  return root_dir == "/Users/yoranbrondsema/Projects/Sutori/sutori/backend"
+end
+
 return {
   "neovim/nvim-lspconfig",
   opts = {
@@ -11,6 +15,21 @@ return {
         enabled = true,
         cmd = { "bundle", "exec", "srb", "tc", "--lsp" },
         root_dir = lspconfig.util.root_pattern("Gemfile", ".git"),
+        on_new_config = function(new_config, root_dir)
+          -- disable for Sutori cause it's causing problems
+          if is_sutori_backend(root_dir) then
+            new_config.enabled = false
+          end
+        end,
+      },
+      ruby_lsp = {
+        enabled = true,
+        on_new_config = function(new_config, root_dir)
+          -- disable for Sutori cause it's causing problems
+          if is_sutori_backend(root_dir) then
+            new_config.enabled = false
+          end
+        end,
       },
       solargraph = {
         enabled = false,
